885d10f38a2d99c53d45e9eb90ad775eae93f45d
Wintrel <discoloredxd@gmail.com>
2025-11-14 09:35:16 +0200

    added history

--- app.js
+++ app.js
@@ -1,13 +1,49 @@
-input = document.getElementById("inputnum")
+inputbar = document.getElementById("inputnum")
 button = document.getElementById("guessbtn")
 resultDiv = document.getElementById("result")
+historyDiv = document.getElementById("history")
+historyList = document.getElementById("historyList")
 
 button.addEventListener("click", guessNumber)
 
+function genereateRandomNumber() {
+    return Math.floor(Math.random() * 100) + 1
+}
+
+let RandomNumber = genereateRandomNumber()
+console.log(RandomNumber)
+
+
 
 function guessNumber() {
-    UserGuess = Number(input.value)
-    RandomNumber = Math.floor(Math.random() * 100) + 1
+    UserGuess = Number(inputbar.value)
+    let UserAttempts = 0
 
-    if (UserGuess === RandomNum)
+    switch (true) {
+        case (UserGuess === RandomNumber):
+            resultDiv.innerHTML = `guessed the number ${RandomNumber} correctly in ${UserAttempts} attempts.`
+            RandomNumber = genereateRandomNumber()
+            console.log("guessed the number")
+            UserAttempts + 1
+            break
+        case (UserGuess < RandomNumber):
+            resultDiv.innerHTML = "Too low"
+            console.log("too low")
+            UserAttempts + 1
+            break
+        case (UserGuess > RandomNumber):
+            resultDiv.innerHTML = "Too high"
+            console.log("too high")
+            UserAttempts + 1
+            break
+    }
+
+    history()
+}
+
+
+function history() {
+    const listItem = document.createElement("li")
+    listItem.textContent = `Guessed: ${UserGuess}`
+    historyList.appendChild(listItem)
 }
